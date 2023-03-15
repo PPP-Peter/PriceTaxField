57210ec83767948eb5ce1d3b086b00bf79e71789
Peter <p.petermanik@gmail.com>
2023-03-15 22:25:03 +0100

    version 1.1 - added one column

--- README.md
+++ README.md
@@ -11,53 +11,41 @@ composer require wamesk/laravel-nova-price-tax-field
 ## Usage
 Add to your nova model <br>
 <small>
-(Helper = field, you can change sort field but getPriceTaxField must be last, in migration use decimal or double)
+(You can change sort field but getPriceTaxField must be last, in migration use decimal or double)
 </small>
 
-
-
-``` php
-use Wame\PriceTaxField\Helpers;
-
-// Price tax field options
-$db_price_without_tax = 'price';
-$db_tax = 'tax';
-$db_price_with_tax = 'price_with_tax';
-$names = ['bez DPH', 'daň', 's DPH'];
-$base_field_name = 'Cena';
-$default_tax = 20;
-        
-Helpers::getPriceWithoutTax($base_field_name, $db_price_without_tax),
-Helpers::getPriceTax($names[1], $db_tax)->rules('required'),
-Helpers::getPriceWithTax($names[2], $db_price_with_tax)->rules('required'),
-Helpers::getPriceTaxField($base_field_name, $db_price_without_tax, $names, $db_tax, $db_price_with_tax, $default_tax)->rules('required'),
-```
-<br>
-
-### OR register alias
-
+#### Add Alias
 ``` php
 'PriceHelper' => Wame\PriceTaxField\PriceField::class,
 ```
 
+#### Add to your models
 ``` php
 use Wame\PriceTaxField\PriceField;
 
 // Price tax field options
-$db_price_without_tax = 'price';
-$db_tax = 'tax';
-$db_price_with_tax = 'price_with_tax';
-$names = ['bez DPH', 'daň', 's DPH'];
-$base_field_name = 'Cena';
-$default_tax = 20;
-
-PriceField::getPriceWithoutTax($base_field_name, $db_price_without_tax),
-PriceField::getPriceTax($names[1], $db_tax)->rules('required'),
-PriceField::getPriceWithTax($names[2], $db_price_with_tax)->rules('required'),
-PriceField::getPriceTaxField($base_field_name, $db_price_without_tax, $names, $db_tax, $db_price_with_tax, $default_tax)->rules('required'),
+$price_tax_options = [
+    'db_names'          => ['base_price', 'tax', 'price_with_tax'],
+    'names'             => ['bez DPH', 'daň', 's DPH'],
+    'base_field_name'   => 'Cena',
+    'default_tax'       => 20,
+    'one_col'           => true,  // display all fields in one column
+    'tax_in_one_col'    => true,  // not show tax value in one column
+    'col_classes'       => ['strong', 'small', 'small'],  // classes for rows in column
+];
+        
+PriceField::getPriceWithoutTax( $price_tax_options, $this),
+PriceField::getPriceTax($price_tax_options)->rules('required'),
+PriceField::getPriceWithTax($price_tax_options)->rules('required'),
+PriceField::getPriceTaxField($price_tax_options)->rules('required'),
 ```
 
-### OR use this METHOD for more custom edits
+<img alt="preview" src="img2.png">
+
+
+<br><br>
+
+### OR you can use this METHOD for more custom edits
 
 ``` php
 use Wame\PriceTaxField\PriceTaxField;
@@ -69,6 +57,8 @@ $db_price_with_tax = 'price_with_tax';
 $names = ['bez DPH', 'daň', 's DPH'];
 $base_field_name = 'Cena';
 $default_tax = 20;
+$one_col = true;  // display all fields in one column
+$tax_in_one_col = true;
 
 Number::make($base_field_name, $db_price_without_tax)->displayUsing(function ($value) {
     return number_format((float)$value, 2, '.', '') . ' €';
@@ -85,6 +75,7 @@ PriceTaxField::make($base_field_name, $db_price_without_tax)->fullWidth()->displ
 })->names($names)->db_names( [$db_price_without_tax, $db_tax,  $db_price_with_tax])->hideFromIndex()->hideFromDetail()
     ->defaultTax($default_tax)->rules('required'),
 ```
+
 
 ## Licence
 
--- src/PriceField.php
+++ src/PriceField.php
@@ -3,65 +3,143 @@
 namespace Wame\PriceTaxField;
 
 use Laravel\Nova\Fields\Number;
-//use Wame\PriceTaxField\PriceTaxField;
+use Laravel\Nova\Http\Requests\NovaRequest;
 
 
 class PriceField
 {
 
     /**
-     * @param string $base_field_name
-     * @param string $db_price_without_tax
-     * @param array $names
-     * @param string $db_tax
-     * @param string $db_price_with_tax
-     * @param int $default_tax
+     * PRICE WITHOUT TAX
+     *
+     * @param $values
+     * @param $options
+     * @return Number
+     */
+    public static function getPriceWithoutTax($options, $values): Number
+    {
+        $base_field_name = $options['base_field_name'];
+        $db_price_without_tax = $options['db_names'][0];
+        $names = $options['names'];
+        $db_names = $options['db_names'];
+        $one_col = $options['one_col'];
+        $classes = $options['col_classes'];
+        $tax_in_one_col = $options['tax_in_one_col'];
+
+        return Number::make($base_field_name, $db_price_without_tax)
+            //->displayUsing(fn () => $values->tax)
+            ->displayUsing(function ($value) use ($one_col, $tax_in_one_col, $names, $db_names, $classes, $values) {
+                if ($one_col === false) {
+                    return self::to_currency($value);
+                }
+                else {
+                    $tax_name = $db_names[1];
+                    $with_tax_name = $db_names[2];
+                    $rows = [];
+                    $rows[] = "<$classes[0]>" . self::to_currency($value) . '<small> ' . $names[0] . "</small></$classes[0]> ";
+                    $rows[] = "<$classes[1]>" . self::to_currency($values->$with_tax_name) . ' ' . $names[2] . "</$classes[1]>";
+                    if ($tax_in_one_col) $rows[] = "<$classes[2]>" . $values->$tax_name .  ' % ' . $names[1] . "</$classes[2]>";
+                    return implode('</br>', $rows);
+                }
+            })
+            ->asHtml()
+            ->hideWhenCreating()
+            ->hideWhenUpdating()
+            ->step(0.01);
+    }
+
+    /**
+     * TAX
+     *
+     * @param $options
+     * @return Number
+     */
+    public static function getPriceTax($options): Number
+    {
+        $name = $options['names'][1];
+        $db_tax = $options['db_names'][1];
+        $one_col = $options['one_col'];
+
+        return Number::make($name, $db_tax)
+            ->displayUsing(function ($value) {
+                return $value . ' %';
+            })
+            ->showOnIndex(function (NovaRequest $request, $resource) use ($one_col){
+                if ($one_col===false) return true ;
+                else return false;
+            })
+            ->showOnDetail(function (NovaRequest $request, $resource) use ($one_col){
+                if ($one_col===false) return true ;
+                else return false;
+            });
+    }
+
+    /**
+     * PRICE WITH TAX
+     *
+     * @param $options
+     * @return Number
+     */
+    public static function getPriceWithTax($options): Number
+    {
+        $name = $options['names'][2];
+        $db_price_with_tax = $options['db_names'][2];
+        $one_col = $options['one_col'];
+
+        return Number::make($name, $db_price_with_tax)
+            ->displayUsing(function ($value) {
+                return self::to_currency($value);
+            })
+            ->showOnIndex(function (NovaRequest $request, $resource) use ($one_col){
+                if ($one_col===false) return true ;
+                else return false;
+            })
+            ->showOnDetail(function (NovaRequest $request, $resource) use ($one_col){
+                if ($one_col===false) return true ;
+                else return false;
+            })
+            ->step(0.01);
+    }
+
+    /**
+     * CUSTOM PRICE FIELD
+     *
+     *  @param $options
      * @return PriceTaxField
      */
-    public static function getPriceTaxField(string $base_field_name, string $db_price_without_tax, array $names, string $db_tax, string $db_price_with_tax, int $default_tax): PriceTaxField
+    public static function getPriceTaxField($options): PriceTaxField
     {
-        return PriceTaxField::make($base_field_name, $db_price_without_tax)->fullWidth()->displayUsing(function ($value) {
-            return number_format((float)$value, 2, '.', '') . ' €';
-        })->names($names)->db_names([$db_price_without_tax, $db_tax, $db_price_with_tax])->hideFromIndex()->hideFromDetail()
+        $base_field_name = $options['base_field_name'];
+        $names = $options['names'];
+        $db_price_with_tax = $options['db_names'][2];
+        $db_price_without_tax = $options['db_names'][0];
+        $db_tax = $options['db_names'][1];
+        $default_tax =  $options['default_tax'];
+
+        return PriceTaxField::make($base_field_name, $db_price_without_tax)
+            ->fullWidth()
+            ->displayUsing(function ($value) {
+                return self::to_currency($value);
+            })
+            ->names($names)
+            ->db_names([$db_price_without_tax, $db_tax, $db_price_with_tax])
+            ->hideFromIndex()
+            ->hideFromDetail()
             ->defaultTax($default_tax);
     }
 
     /**
-     * @param $names
-     * @param string $db_price_with_tax
-     * @return Number
+     * @param $value
+     * @return string
      */
-    public static function getPriceWithTax($names, string $db_price_with_tax): Number
+    private static function to_currency($value): string
     {
-        return Number::make($names, $db_price_with_tax)->displayUsing(function ($value) {
-            return number_format((float)$value, 2, '.', '') . ' €';
-        })->step(0.01);
+        return number_format((float)$value, 2, '.', '') . ' €';
     }
 
-    /**
-     * @param $names
-     * @param string $db_tax
-     * @return Number
-     */
-    public static function getPriceTax($names, string $db_tax): Number
-    {
-        return Number::make($names, $db_tax)->displayUsing(function ($value) {
-            return $value . ' %';
-        });
-    }
-
-    /**
-     * @param string $base_field_name
-     * @param string $db_price_without_tax
-     * @return Number
-     */
-    public static function getPriceWithoutTax(string $base_field_name, string $db_price_without_tax): Number
-    {
-        return Number::make($base_field_name, $db_price_without_tax)->displayUsing(function ($value) {
-            return number_format((float)$value, 2, '.', '') . ' €';
-        })->step(0.01)->hideWhenCreating()->hideWhenUpdating();
-    }
 
 }
+
+
 
 
